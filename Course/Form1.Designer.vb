7c64e90fbcc5931082c91dbf9b6540da574d46d1
Виталий Козюлин <qargvo@gmail.com>
2022-03-24 23:50:55 +0300

    1101

--- Course/Form1.Designer.vb
+++ Course/Form1.Designer.vb
@@ -22,16 +22,45 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.cmd_exit = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'cmd_exit
+        '
+        Me.cmd_exit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.cmd_exit.Location = New System.Drawing.Point(37, 335)
+        Me.cmd_exit.Name = "cmd_exit"
+        Me.cmd_exit.Size = New System.Drawing.Size(140, 94)
+        Me.cmd_exit.TabIndex = 0
+        Me.cmd_exit.Text = "Exit"
+        Me.cmd_exit.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button1.Location = New System.Drawing.Point(454, 335)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 93)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(843, 478)
+        Me.ClientSize = New System.Drawing.Size(638, 478)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cmd_exit)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Курсовая работа"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents cmd_exit As Button
+    Friend WithEvents Button1 As Button
 End Class
--- Course/Form1.vb
+++ Course/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub cmd_exit_Click(sender As Object, e As EventArgs) Handles cmd_exit.Click
+        End
+    End Sub
 End Class
