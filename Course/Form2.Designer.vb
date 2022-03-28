4412430e964a6278a672f7f77eb06360f50fb646
Виталий Козюлин <qargvo@gmail.com>
2022-03-28 23:30:33 +0300

    ErrorType msg

--- Course/Form2.Designer.vb
+++ Course/Form2.Designer.vb
@@ -24,13 +24,17 @@ Partial Class frm_main
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_main))
-        Me.btn_exit = New System.Windows.Forms.Button()
-        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.NotifyIcon2 = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
-        Me.NotifyIcon3 = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.menustr = New System.Windows.Forms.MenuStrip()
+        Me.pnl_input = New System.Windows.Forms.Panel()
+        Me.lbl_x3 = New System.Windows.Forms.Label()
+        Me.lbl_x2 = New System.Windows.Forms.Label()
+        Me.lbl_x1 = New System.Windows.Forms.Label()
+        Me.txt_x3 = New System.Windows.Forms.TextBox()
+        Me.txt_x2 = New System.Windows.Forms.TextBox()
+        Me.txt_x1 = New System.Windows.Forms.TextBox()
+        Me.txt_output = New System.Windows.Forms.TextBox()
+        Me.btn_calc = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Error_input = New System.Windows.Forms.ErrorProvider(Me.components)
         Me.FIleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.СохранитьToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.СохранитькакToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -44,47 +48,107 @@ Partial Class frm_main
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutMeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutProgToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menustr = New System.Windows.Forms.MenuStrip()
+        Me.pnl_input.SuspendLayout()
+        CType(Me.Error_input, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.menustr.SuspendLayout()
         Me.SuspendLayout()
         '
-        'btn_exit
+        'pnl_input
         '
-        Me.btn_exit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
-        Me.btn_exit.Location = New System.Drawing.Point(12, 398)
-        Me.btn_exit.Name = "btn_exit"
-        Me.btn_exit.Size = New System.Drawing.Size(79, 40)
-        Me.btn_exit.TabIndex = 0
-        Me.btn_exit.Text = "Exit"
-        Me.btn_exit.UseVisualStyleBackColor = True
+        Me.pnl_input.Controls.Add(Me.txt_x3)
+        Me.pnl_input.Controls.Add(Me.txt_x2)
+        Me.pnl_input.Controls.Add(Me.txt_x1)
+        Me.pnl_input.Location = New System.Drawing.Point(82, 104)
+        Me.pnl_input.Name = "pnl_input"
+        Me.pnl_input.Size = New System.Drawing.Size(98, 113)
+        Me.pnl_input.TabIndex = 8
         '
-        'NotifyIcon1
+        'lbl_x3
         '
-        Me.NotifyIcon1.Text = "NotifyIcon1"
-        Me.NotifyIcon1.Visible = True
+        Me.lbl_x3.AutoSize = True
+        Me.lbl_x3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lbl_x3.Location = New System.Drawing.Point(42, 184)
+        Me.lbl_x3.Name = "lbl_x3"
+        Me.lbl_x3.Size = New System.Drawing.Size(34, 24)
+        Me.lbl_x3.TabIndex = 5
+        Me.lbl_x3.Text = "X3"
         '
-        'NotifyIcon2
+        'lbl_x2
         '
-        Me.NotifyIcon2.Text = "NotifyIcon2"
-        Me.NotifyIcon2.Visible = True
+        Me.lbl_x2.AutoSize = True
+        Me.lbl_x2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lbl_x2.Location = New System.Drawing.Point(42, 149)
+        Me.lbl_x2.Name = "lbl_x2"
+        Me.lbl_x2.Size = New System.Drawing.Size(34, 24)
+        Me.lbl_x2.TabIndex = 4
+        Me.lbl_x2.Text = "X2"
         '
-        'NotifyIcon3
+        'lbl_x1
         '
-        Me.NotifyIcon3.Text = "NotifyIcon3"
-        Me.NotifyIcon3.Visible = True
+        Me.lbl_x1.AutoSize = True
+        Me.lbl_x1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lbl_x1.Location = New System.Drawing.Point(42, 114)
+        Me.lbl_x1.Name = "lbl_x1"
+        Me.lbl_x1.Size = New System.Drawing.Size(34, 24)
+        Me.lbl_x1.TabIndex = 3
+        Me.lbl_x1.Text = "X1"
         '
-        'ContextMenuStrip1
+        'txt_x3
         '
-        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        Me.txt_x3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.txt_x3.Location = New System.Drawing.Point(3, 77)
+        Me.txt_x3.Name = "txt_x3"
+        Me.txt_x3.Size = New System.Drawing.Size(90, 29)
+        Me.txt_x3.TabIndex = 2
         '
-        'menustr
+        'txt_x2
         '
-        Me.menustr.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FIleToolStripMenuItem, Me.CalcToolStripMenuItem, Me.ToolsToolStripMenuItem, Me.HelpToolStripMenuItem})
-        Me.menustr.Location = New System.Drawing.Point(0, 0)
-        Me.menustr.Name = "menustr"
-        Me.menustr.Size = New System.Drawing.Size(800, 24)
-        Me.menustr.TabIndex = 7
-        Me.menustr.Text = "MenuStrip2"
+        Me.txt_x2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.txt_x2.Location = New System.Drawing.Point(3, 42)
+        Me.txt_x2.Name = "txt_x2"
+        Me.txt_x2.Size = New System.Drawing.Size(90, 29)
+        Me.txt_x2.TabIndex = 1
+        '
+        'txt_x1
+        '
+        Me.txt_x1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.txt_x1.Location = New System.Drawing.Point(3, 7)
+        Me.txt_x1.Name = "txt_x1"
+        Me.txt_x1.Size = New System.Drawing.Size(90, 29)
+        Me.txt_x1.TabIndex = 0
+        '
+        'txt_output
+        '
+        Me.txt_output.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.txt_output.Location = New System.Drawing.Point(352, 143)
+        Me.txt_output.Name = "txt_output"
+        Me.txt_output.Size = New System.Drawing.Size(118, 29)
+        Me.txt_output.TabIndex = 9
+        '
+        'btn_calc
+        '
+        Me.btn_calc.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.btn_calc.Location = New System.Drawing.Point(352, 183)
+        Me.btn_calc.Name = "btn_calc"
+        Me.btn_calc.Size = New System.Drawing.Size(118, 34)
+        Me.btn_calc.TabIndex = 10
+        Me.btn_calc.Text = "Расчет"
+        Me.btn_calc.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(277, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(309, 50)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Расчет  производительности вакуум-фильтра по сухому веществу"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Error_input
+        '
+        Me.Error_input.ContainerControl = Me
         '
         'FIleToolStripMenuItem
         '
@@ -98,19 +162,19 @@ Partial Class frm_main
         Me.СохранитьToolStripMenuItem.Image = CType(resources.GetObject("СохранитьToolStripMenuItem.Image"), System.Drawing.Image)
         Me.СохранитьToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.СохранитьToolStripMenuItem.Name = "СохранитьToolStripMenuItem"
-        Me.СохранитьToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.СохранитьToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
         Me.СохранитьToolStripMenuItem.Text = "&Сохранить"
         '
         'СохранитькакToolStripMenuItem
         '
         Me.СохранитькакToolStripMenuItem.Name = "СохранитькакToolStripMenuItem"
-        Me.СохранитькакToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.СохранитькакToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
         Me.СохранитькакToolStripMenuItem.Text = "Сохранить &как"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
         Me.ExitToolStripMenuItem.Text = "Вы&ход"
         '
         'CalcToolStripMenuItem
@@ -170,28 +234,50 @@ Partial Class frm_main
         Me.AboutProgToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
         Me.AboutProgToolStripMenuItem.Text = "О программе"
         '
+        'menustr
+        '
+        Me.menustr.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FIleToolStripMenuItem, Me.CalcToolStripMenuItem, Me.ToolsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.menustr.Location = New System.Drawing.Point(0, 0)
+        Me.menustr.Name = "menustr"
+        Me.menustr.Size = New System.Drawing.Size(631, 24)
+        Me.menustr.TabIndex = 7
+        Me.menustr.Text = "MenuStrip2"
+        '
         'frm_main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(631, 388)
+        Me.Controls.Add(Me.lbl_x3)
+        Me.Controls.Add(Me.lbl_x2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_x1)
+        Me.Controls.Add(Me.btn_calc)
+        Me.Controls.Add(Me.txt_output)
+        Me.Controls.Add(Me.pnl_input)
         Me.Controls.Add(Me.menustr)
-        Me.Controls.Add(Me.btn_exit)
         Me.Name = "frm_main"
         Me.Text = "Курсовая"
+        Me.pnl_input.ResumeLayout(False)
+        Me.pnl_input.PerformLayout()
+        CType(Me.Error_input, System.ComponentModel.ISupportInitialize).EndInit()
         Me.menustr.ResumeLayout(False)
         Me.menustr.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btn_exit As Button
-    Friend WithEvents NotifyIcon1 As NotifyIcon
-    Friend WithEvents NotifyIcon2 As NotifyIcon
-    Friend WithEvents ToolTip1 As ToolTip
-    Friend WithEvents NotifyIcon3 As NotifyIcon
-    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents pnl_input As Panel
+    Friend WithEvents txt_x1 As TextBox
+    Friend WithEvents txt_x3 As TextBox
+    Friend WithEvents txt_x2 As TextBox
+    Friend WithEvents lbl_x3 As Label
+    Friend WithEvents lbl_x2 As Label
+    Friend WithEvents lbl_x1 As Label
+    Friend WithEvents txt_output As TextBox
+    Friend WithEvents btn_calc As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Error_input As ErrorProvider
     Friend WithEvents menustr As MenuStrip
     Friend WithEvents FIleToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents СохранитьToolStripMenuItem As ToolStripMenuItem
--- Course/Form2.vb
+++ Course/Form2.vb
@@ -1,9 +1,42 @@
 ﻿Public Class frm_main
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_exit.Click
-        End
-    End Sub
+    Dim input_x(3) As Integer
 
     Private Sub ВыходToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         End
     End Sub
+
+    Private Sub btn_calc_Click(sender As Object, e As EventArgs) Handles btn_calc.Click
+        Dim inp As String
+        Dim error_string As String
+        Dim error_flag As Boolean
+        Dim i As Integer
+
+        error_string = ""
+        error_flag = False
+
+
+
+        For i = 0 To 2
+            inp = pnl_input.Controls(i).Text
+            If validate_as_num(inp) Then
+                input_x(i) = Val(inp)
+            Else
+                error_string += "X" + CStr(i + 1) + " "
+                error_flag = True
+            End If
+        Next i
+
+        If error_flag Then
+            error_string = "Неверный тип значения " + error_string
+            MsgBox(error_string, MsgBoxStyle.OkOnly Or MsgBoxStyle.Critical)
+        End If
+    End Sub
+
+    Public Function validate_as_num(a) As Boolean
+        If IsNumeric(a) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
 End Class
